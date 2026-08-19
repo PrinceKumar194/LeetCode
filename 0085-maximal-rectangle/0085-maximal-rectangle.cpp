9088b348c11e07b2216f152dc57272bf0d1f4f31
cout"Prince Kumar <meprincedude@gmail.com>
2026-08-19 23:06:10 +0530

    Time: 12 ms (33.97%), Space: 23.3 MB (14.23%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,88 @@
+class Solution {
+public:
+
+
+    int largestRectangleArea(vector<int>& heights) {
+
+        vector<int>right(heights.size());
+        vector<int>left(heights.size());
+        stack<int>st;
+
+        for(int i=0;i<heights.size();i++){
+
+            while(!st.empty() && heights[st.top()]>heights[i]){
+
+                right[st.top()]=i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            right[st.top()]=heights.size();
+            st.pop();
+        }
+
+
+        // Next smallest left
+
+        for(int i=heights.size()-1;i>=0;i--){
+
+
+            while(!st.empty() && heights[st.top()]>heights[i]){
+
+                left[st.top()]=i;
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()){
+
+            left[st.top()]=-1;
+            st.pop();
+        }
+
+
+        // finding ans
+
+        vector<int>ans(heights.size());
+
+        for(int i=0;i<heights.size();i++){
+
+            ans[i]=heights[i]*(right[i]-left[i]-1);
+
+        }
+
+        return *max_element(ans.begin(), ans.end());
+        
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    int ans = 0;
+
+    vector<int> height(col, 0);
+
+    for(int i = 0; i < row; i++) {
+
+        for(int j = 0; j < col; j++) {
+
+            if(matrix[i][j] == '0') {
+                height[j] = 0;
+            }
+            else {
+                height[j] += 1;
+            }
+        }
+
+        ans = max(ans, largestRectangleArea(height));
+    }
+
+    return ans;
+}
+
+        
+    
+};
