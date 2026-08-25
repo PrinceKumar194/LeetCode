9f21d4ced90381e00651d59f0aea2a41b1a40976
cout"Prince Kumar <meprincedude@gmail.com>
2026-08-25 21:49:50 +0530

    Time: 0 ms (100%), Space: 10.1 MB (77.94%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+
+        int sum = 0;
+        int size = s.size();
+
+        for (int i = 0; i < size; i++) {
+
+            int current;
+
+            if (s[i] == 'I')
+                current = 1;
+            else if (s[i] == 'V')
+                current = 5;
+            else if (s[i] == 'X')
+                current = 10;
+            else if (s[i] == 'L')
+                current = 50;
+            else if (s[i] == 'C')
+                current = 100;
+            else if (s[i] == 'D')
+                current = 500;
+            else
+                current = 1000;
+
+            if (i + 1 < size) {
+
+                int next;
+
+                if (s[i + 1] == 'I')
+                    next = 1;
+                else if (s[i + 1] == 'V')
+                    next = 5;
+                else if (s[i + 1] == 'X')
+                    next = 10;
+                else if (s[i + 1] == 'L')
+                    next = 50;
+                else if (s[i + 1] == 'C')
+                    next = 100;
+                else if (s[i + 1] == 'D')
+                    next = 500;
+                else
+                    next = 1000;
+
+                if (current < next)
+                    sum -= current;
+                else
+                    sum += current;
+            }
+            else {
+                sum += current;
+            }
+        }
+
+        return sum;
+    }
+};
