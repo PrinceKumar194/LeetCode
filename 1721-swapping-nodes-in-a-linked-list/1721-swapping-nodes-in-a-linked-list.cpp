1d536f212be9e664cd2972dc30467835d8b931a0
cout"Prince Kumar <meprincedude@gmail.com>
2026-08-27 11:18:25 +0530

    Time: 0 ms (100%), Space: 185.3 MB (39.53%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -12,34 +12,61 @@ class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
 
-        vector<int>ans;
-        ListNode*curr;
-        curr=head;
+        // vector<int>ans;
+        // ListNode*curr;
+        // curr=head;
+
+        // while(curr!=NULL){
+
+        //     ans.push_back(curr->val);
+        //     curr=curr->next;
+        // }
+        // int first=0;
+        // int last=ans.size()-1;
+
+        // for(int i=0;i<k-1;i++){
+        //     first++;
+        //     last--;
+        // }
+
+        // int temp=ans[first];
+        // ans[first]=ans[last];
+        // ans[last]=temp;
+
+        // curr=head;
+
+        // for(int i=0;i<ans.size();i++){
+
+        //     curr->val=ans[i];
+        //     curr=curr->next;
+        // }
+
+        // return head;
+
+        // ANOTHER METHOD
+
+        int len=0;
+        ListNode*curr=head;
 
         while(curr!=NULL){
-
-            ans.push_back(curr->val);
+            len++;
             curr=curr->next;
         }
-        int first=0;
-        int last=ans.size()-1;
+
+        ListNode*first=head;
+        ListNode*last=head;
 
         for(int i=0;i<k-1;i++){
-            first++;
-            last--;
+            first=first->next;
         }
 
-        int temp=ans[first];
-        ans[first]=ans[last];
-        ans[last]=temp;
-
-        curr=head;
-
-        for(int i=0;i<ans.size();i++){
-
-            curr->val=ans[i];
-            curr=curr->next;
+        for(int j=0;j<len-k;j++){
+            last=last->next;
         }
+
+        int temp=first->val;
+        first->val=last->val;
+        last->val=temp;
 
         return head;
         
